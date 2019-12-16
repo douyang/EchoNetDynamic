df45f5aabbf45ed131ed4f5ea5b889b86ca9aa2a
Bryan He <bryanhe@stanford.edu>
2019-12-15 16:51:24 -0800

    add training patients sweep

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -32,3 +32,18 @@ do
 done
 
 python3 -c "import echonet; echonet.utils.segmentation.run(modelname=\"deeplabv3_resnet50\",  save_segmentation=True, pretrained=False)"
+
+pretrained=True
+model=r2plus1d_18
+period=2
+batch=$((256 / 64 * period))
+batch=$(( batch > 16 ? 16 : batch ))
+for patients in 16 32 64 128 256 512 1024 2048 4096 7460
+do
+    cmd="import echonet; echonet.utils.video.run(modelname=\"${model}\", frames=(64 // ${period}), period=${period}, pretrained=${pretrained}, batch_size=${batch}, num_epochs=min(50 * (8192 // ${patients}), 200), output=\"output/training_size/video/${patients}\", n_train_patients=${patients})"
+    python3 -c "${cmd}"
+    cmd="import echonet; echonet.utils.segmentation.run(modelname=\"deeplabv3_resnet50\", pretrained=False, num_epochs=min(50 * (8192 // ${patients}), 200), output=\"output/training_size/segmentation/${patients}\", n_train_patients=${patients})"
+    python3 -c "${cmd}"
+
+done
+
